3a75538152875b383a77d95023bc0ad6c13a3787
keerthi1293 <133513211+keerthi1293@users.noreply.github.com>
2025-06-30 14:00:53 +0530

    Create Hero.jsx

---
+++ Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <section className="w-full py-24 text-center bg-white">
+      <h2 className="text-4xl font-bold mb-4">Hi, I'm Keerthi S</h2>
+      <p className="text-lg">AI & ML Enthusiast | Software & Cloud Developer</p>
+    </section>
+  );
+};
+
+export default Hero;
